0ac204d1bec4b0a9d9c5f3d20af90b349cec6fde
araschain <76245221+araschain@users.noreply.github.com>
2023-08-15 22:16:11 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,7 @@
 export default {
-  channelId: "1120762224311549962",
   discord: {
     token:
-      "Nzc3NTk1Mjk1MDU1MDg1NTg4.GWzkqF.WZjIS8yL9t2xO6Z4We17R9DMda5zoN5yFc9CVA",
-    id: "777595295055085588",
+      "token",
+    id: "sunucuid",
   },
 };
